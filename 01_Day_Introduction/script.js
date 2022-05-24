94ec94183c822f3d715721e4ff76067f5ad70169
batuhankendirli <76517857+batuhankendirli@users.noreply.github.com>
2022-05-24 20:40:46 +0300

    Day 1

---
+++ 01_Day_Introduction/script.js
@@ -0,0 +1,63 @@
+// EXERCISES
+
+// 1
+// comments can make code readable
+
+// 2
+// Welcome to 30DaysOfJavaScript
+
+// 3
+/*
+comments can make code readable,
+easy to reuse and informative
+*/
+
+// 4
+const someString = 'Heyy';
+let someBoolean = false;
+let x = undefined;
+let y; // also undefined
+let z = null;
+
+// 5
+console.log(
+  typeof someString,
+  typeof someBoolean,
+  typeof x,
+  typeof y,
+  typeof z
+);
+
+// 6
+let variable1;
+let variable2;
+let variable3;
+let variable4;
+
+// 7
+const variable5 = 'Turkey';
+const variable6 = 'Istanbul';
+let variable7 = 17;
+let variable8 = true;
+
+// 8
+const firstName = 'Batuhan';
+const lastName = 'Kendirli';
+let isMarried = false;
+const country = 'Turkey';
+let age = 22;
+
+// 9
+let firstName1 = 'Batuhan',
+  lastName1 = 'Kendirli',
+  isMarried1 = false,
+  country1 = 'Turkey',
+  age1 = 22;
+
+//10
+
+let myAge = 22;
+let yourAge = 47;
+console.log(`I am ${myAge} years old.\nYou are ${yourAge} years old.`);
+
+// END OF THE EXCERCISES
